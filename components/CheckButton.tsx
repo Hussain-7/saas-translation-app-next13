9576048e20f0ddc9391800587456b43f5b119e43
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-12-13 02:21:02 +0500

    pricing page completed

---
+++ components/CheckButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CheckButton = () => {
+	return (
+		<div>CheckButton</div>
+	)
+}
+
+export default CheckButton
--- components/PricingCards.tsx
+++ components/PricingCards.tsx
@@ -1,5 +1,7 @@
 import { CheckIcon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
+import CheckButton from "./CheckButton";
 const tiers = [
   {
     name: "Starter",
@@ -88,6 +90,18 @@ const PricingCards = ({ redirect }: { redirect: boolean }) => {
                 ))}
               </ul>
             </div>
+            {redirect ? (
+              <Link
+                href="/register"
+                className="mt-8 block rounded-md text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline-2 focus-visible: outline-offset-2 cursor-pointer disabled: opacity-80 bg-indigo-600 px-3.5 py-2 text-center hover:bg-indigo-500 focus-visible: outline focus-visible:outline-indigo-600
+							
+							"
+              >
+                Get Started Today
+              </Link>
+            ) : (
+              tier.id && <CheckButton />
+            )}
           </div>
         ))}
       </div>
