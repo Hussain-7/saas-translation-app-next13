258f93588e51e028641d3bdb00b64343f21cb1af
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-12-13 00:20:58 +0500

    basic chat button added

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,8 @@ import UserButton from "./UserButton";
 import { getServerSession } from "next-auth";
 import { getServerSessionCustom } from "@/auth";
 import Link from "next/link";
-import { MessageSquareIcon } from "lucide-react";
+import { MessagesSquareIcon } from "lucide-react";
+import CreateChatButton from "./CreateChatButton";
 
 type Props = {};
 
@@ -16,15 +17,16 @@ const Header = async (props: Props) => {
   console.log("session", session);
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
-      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
+      <nav className="flex flex-col gap-2 sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
         <Logo />
         <div className="flex-1 flex items-center justify-end space-x-4">
           {/* language select */}
           {session ? (
             <>
               <Link href={"/chat"} prefetch={false}>
-                <MessageSquareIcon className="w-4 h-4 text-black dark:text-white" />
+                <MessagesSquareIcon />
               </Link>
+              <CreateChatButton />
             </>
           ) : (
             <Link href={"/pricing"}>Pricing</Link>
