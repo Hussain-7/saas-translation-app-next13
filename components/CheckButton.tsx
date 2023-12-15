aae3446714680d5947cc8c02dee45f53e6b25561
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-12-16 01:12:38 +0500

    subscriptions working now

--- components/CheckButton.tsx
+++ components/CheckButton.tsx
@@ -1,13 +1,39 @@
 "use client";
+import { db } from "@/firebase";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useState } from "react";
 
 const CheckoutButton = () => {
   const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
   const createCheckoutSession = async () => {
     if (!session) return;
+    setLoading(true);
     // push a document into firestore db
+    const docRef = await addDoc(
+      collection(db, "customers", session.user.id, "checkout_sessions"),
+      {
+        price: "price_1ONhAIF74vorh76ZWBTfkFd8",
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      }
+    );
     // .... stripe extension on firebase will create checkout session
+    return onSnapshot(docRef, (snap) => {
+      const data = snap.data();
+      const url = data?.url;
+      const error = data?.error;
+      if (error) {
+        alert(`An error occured: ${error.message}`);
+        setLoading(false);
+      }
+      if (url) {
+        window.location.assign(url);
+        setLoading(false);
+      }
+    });
+
     // redirect user to checkout page
   };
   return (
@@ -17,7 +43,7 @@ const CheckoutButton = () => {
         onClick={() => createCheckoutSession()}
         className="mt-8 block rounded-md text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline-2 focus-visible:outline-offset-2 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 bg-indigo-600 px-3.5 py-2 text-center hover:bg-indigo-500 focus-visible:outline focus-visible:outline-indigo-600 disabled:text-white disabled:cursor-default"
       >
-        Sign Up
+        {loading ? "loading..." : "Sign Up"}
       </button>
     </div>
   );
