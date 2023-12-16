98b2296d52f13cfb475cf83072bb56661eb47dd9
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-12-16 16:15:56 +0500

    server actions created

--- components/CheckButton.tsx
+++ components/CheckButton.tsx
@@ -4,9 +4,15 @@ import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import React, { useState } from "react";
 import LoadingSpinner from "./LoadingSpinner";
+import { useSubscriptionStore } from "@/store/store";
+import ManageAccountButton from "./ManageAccountButton";
 
 const CheckoutButton = () => {
   const { data: session } = useSession();
+  const subscription = useSubscriptionStore((state) => state.subscription);
+  const isLoadingSubscription = subscription === undefined;
+  const isSubscribed =
+    subscription?.status === "active" && subscription?.role === "pro";
   const [loading, setLoading] = useState(false);
   const createCheckoutSession = async () => {
     if (!session) return;
@@ -41,12 +47,15 @@ const CheckoutButton = () => {
   return (
     <div className="flex flex-col space-y-2">
       {" "}
-      <button
-        onClick={() => createCheckoutSession()}
-        className="mt-8 block rounded-md text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline-2 focus-visible:outline-offset-2 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 bg-indigo-600 px-3.5 py-2 text-center hover:bg-indigo-500 focus-visible:outline focus-visible:outline-indigo-600 disabled:text-white disabled:cursor-default"
-      >
-        {loading ? <LoadingSpinner /> : "Sign Up"}
-      </button>
+      <div className="mt-8 block rounded-md text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline-2 focus-visible:outline-offset-2 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 bg-indigo-600 px-3.5 py-2 text-center hover:bg-indigo-500 focus-visible:outline focus-visible:outline-indigo-600 disabled:text-white disabled:cursor-default">
+        {isSubscribed ? (
+          <ManageAccountButton />
+        ) : isLoadingSubscription || loading ? (
+          <LoadingSpinner />
+        ) : (
+          <button onClick={() => createCheckoutSession()}>Sign Up</button>
+        )}
+      </div>
     </div>
   );
 };
