3a3c31354768835cdb81d2d9e6b8dde2c40190fc
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-12-17 01:26:04 +0500

    message convertor created

--- components/ChatListRow.tsx
+++ components/ChatListRow.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { Message, limitedSortedMessagesRef } from "@/lib/convertors/Message";
 
 type Props = {
   chatId: string;
 };
 
 const ChatListRow = ({ chatId }: Props) => {
+  const [messages, loading, error] = useCollectionData<Message>(
+    limitedSortedMessagesRef(chatId)
+  );
   return <div>ChatListRow : {chatId}</div>;
 };
 
---
+++ lib/convertors/Message.ts
@@ -0,0 +1,78 @@
+// Convertor are responsible for how we push and pull data in our database
+// so in this case for subscription wee push data to firestore
+
+import { db } from "@/firebase";
+import { LanguagesSupported } from "@/store/store";
+import { Subscription } from "@/types/Subscription";
+import {
+  DocumentData,
+  FirestoreDataConverter,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+  collection,
+  collectionGroup,
+  doc,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+
+export interface User {
+  id: string;
+  email: string;
+  name: string;
+  image: string;
+}
+
+export interface Message {
+  id?: string;
+  input: string;
+  timestamp: string;
+  user: User;
+  translated?: {
+    [K in LanguagesSupported]?: string;
+  };
+}
+
+const messageConvertor: FirestoreDataConverter<Message> = {
+  // when we push data to the firestore well'l push it in the form of a subscription
+  toFirestore: (message: Message): DocumentData => {
+    return {
+      input: message.input,
+      timestamp: message.timestamp,
+      user: message.user,
+      // translated: message.translated,
+    };
+  },
+  // the that we expect to pull out from db
+  fromFirestore: (
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions
+  ): Message => {
+    const data = snapshot.data(options);
+    return {
+      id: snapshot.id,
+      input: data.input,
+      timestamp: data.timestamp,
+      translated: data.translated,
+      user: data.user,
+    };
+  },
+};
+
+export const messagesRef = (chatId: string) =>
+  collection(db, "chats", chatId, "messages").withConverter(
+    messageConvertor
+  );
+
+// For a specfic chat get memebers
+export const limitedMessagesRef = (chatId: string) =>
+  query(messagesRef(chatId), limit(25));
+// orderBy('timestamp','asc')
+
+export const sortedMessageRef = (chatId: string) =>
+  query(messagesRef(chatId), orderBy("timestamp", "asc"));
+
+export const limitedSortedMessagesRef = (chatId: string) =>
+  query(messagesRef(chatId), limit(1), orderBy("timestamp", "desc"));
