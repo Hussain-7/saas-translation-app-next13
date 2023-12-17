17481d098786bc84757fca619911d7cae9a9a447
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-12-18 00:35:39 +0500

    invite flow is done

--- components/InviteUser.tsx
+++ components/InviteUser.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -25,50 +26,167 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { useSession } from "next-auth/react";
+import { useToast } from "./ui/use-toast";
+import useAdminId from "@/hooks/useAdminId";
+import { useSubscriptionStore } from "@/store/store";
+import { useRouter } from "next/navigation";
+import { PlusCircleIcon } from "lucide-react";
+import { getDocs, serverTimestamp, setDoc } from "firebase/firestore";
+import { addChatRef, chatMembersRef } from "@/lib/convertors/ChatMembers";
+import { ToastAction } from "./ui/toast";
+import { getUserByEmailRef } from "@/lib/convertors/User";
+import ShareLink from "./ShareLink";
 
 type Props = {
   chatId: string;
 };
 
+const formSchema = z.object({
+  email: z.string().email("Please enter a valid email address"),
+});
+
 const InviteUser = ({ chatId }: Props) => {
+  const { data: session } = useSession();
+  const { toast } = useToast();
+  const adminId = useAdminId({
+    chatId,
+  });
+  const subscription = useSubscriptionStore((state) => state.subscription);
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [openInviteLink, setOpenInviteLink] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    if (!session?.user?.id) return;
+    toast({
+      title: "Invite Sent",
+      description: `Please wait while we send the invite...`,
+    });
+
+    const noOfUserInChat = (await getDocs(chatMembersRef(chatId))).docs.map(
+      (doc) => doc.data()
+    ).length;
+
+    const isPro =
+      subscription?.role === "pro" && subscription?.status === "active";
+    if (!isPro && noOfUserInChat >= 2) {
+      toast({
+        title: "",
+        description: "",
+        variant: "destructive",
+        action: (
+          <ToastAction
+            altText="Upgrade"
+            onClick={() => router.push("/register")}
+          >
+            Upgrade to PRO
+          </ToastAction>
+        ),
+      });
+      return;
+    }
+
+    const querySnapshot = await getDocs(getUserByEmailRef(values.email));
+    if (querySnapshot.empty) {
+      toast({
+        title: "User not found",
+        description: `Please enter an email address off a registered user OR resed invitation once they have signed up!`,
+        variant: "destructive",
+      });
+      return;
+    } else {
+      const user = querySnapshot.docs[0].data();
+      await setDoc(addChatRef(chatId, user.id), {
+        userId: user.id!,
+        email: user.email!,
+        timestamp: serverTimestamp(),
+        chatId: chatId,
+        isAdmin: false,
+        image: user.image!,
+      })
+        .then(() => {
+          setOpen(false);
+          toast({
+            title: "Added to chat",
+            description: `The user has been added to the chat succesfully!`,
+            duration: 3000,
+            className: "bg-green-600 text-white",
+          });
+          setOpenInviteLink(true);
+        })
+        .catch((err) => {
+          toast({
+            title: "Error",
+            description: `Whoops.. there was error adding the user to the chat!`,
+            variant: "destructive",
+          });
+          setOpen(false);
+        });
+    }
+    form.reset();
+  }
+
   return (
-    <>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline">Invite</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-md">
-          <DialogHeader>
-            <DialogTitle>Share link</DialogTitle>
-            <DialogDescription>
-              Anyone who has this link will be able to view this.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="flex items-center space-x-2">
-            <div className="grid flex-1 gap-2">
-              <Label htmlFor="link" className="sr-only">
-                Link
-              </Label>
-              <Input
-                id="link"
-                defaultValue="https://ui.shadcn.com/docs/installation"
-                readOnly
-              />
-            </div>
-            <Button type="submit" size="sm" className="px-3">
-              Send
+    adminId === session?.user?.id && (
+      <>
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <Button variant="outline">
+              <PlusCircleIcon className="mr-1" />
+              Add User to Chat
             </Button>
-          </div>
-          <DialogFooter className="sm:justify-start">
-            <DialogClose asChild>
-              <Button type="button" variant="secondary">
-                Close
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-md">
+            <DialogHeader>
+              <DialogTitle>Add User to Chat</DialogTitle>
+              <DialogDescription>
+                Simply Enter another users email address to invite them to this
+                chat!{" "}
+                <span className="text-indigo-600 font-bold">
+                  (Note: they must be registered)
+                </span>
+              </DialogDescription>
+            </DialogHeader>
+
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex flex-col space-y-2"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input placeholder="john@doe.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" className="ml-auto sm:w-fit w-full">
+                  Add To Chat
+                </Button>
+              </form>
+            </Form>
+          </DialogContent>
+        </Dialog>
+        <ShareLink
+          isOpen={openInviteLink}
+          setIsOpen={setOpenInviteLink}
+          chatId={chatId}
+        />
+      </>
+    )
   );
 };
 
